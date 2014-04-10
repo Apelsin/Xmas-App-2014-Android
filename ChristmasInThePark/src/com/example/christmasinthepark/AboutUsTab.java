47dd3d62e13cee970437063b4004de70072e67c3
Sir <sir.spensaur@gmail.com>
2014-04-09 22:29:29 -0700

    button

--- ChristmasInThePark/src/com/example/christmasinthepark/AboutUsTab.java
+++ ChristmasInThePark/src/com/example/christmasinthepark/AboutUsTab.java
@@ -1,16 +1,26 @@
 package com.example.christmasinthepark;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutUsTab extends Fragment {
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container, 
 	                           Bundle savedInstanceState){
 		View view = inflater.inflate(R.layout.about, container, false);
+		final Button button = (Button) view.findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q5QS4M4QPR5HE"));
+                startActivity(browserIntent);
+            }
+        });
 //		TextView textview = (TextView) view.findViewById(R.id.tabtextview);
 //		textview.setText(R.string.AboutUs);
 		return view;
